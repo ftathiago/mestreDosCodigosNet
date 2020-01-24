05c5adfe853b16365b3e7e0208bbb97f40dd7ab8
Francisco Thiago de Almeida <ftathiago@gmail.com>
2020-01-24 13:24:37 -0300

    Televisao.Canais

--- Utilizando POO/exercicio04/SalaDeEstar.Test/Televisao.Test.cs
+++ Utilizando POO/exercicio04/SalaDeEstar.Test/Televisao.Test.cs
@@ -9,8 +9,39 @@ namespace SalaDeEstar.Test
         [TestMethod]
         public void ShouldCreateTelevisao()
         {
-            Televisao televisao = new Televisao();
+            var canalInicial = 0;
+            Televisao televisao = new Televisao(canalInicial);
             Assert.IsNotNull(televisao);
+        }
+
+        [TestMethod]
+        public void ShouldChangeToNextChannelIncreasedByOne()
+        {
+            Televisao televisao = PegarTelevisao();
+            var canalAtual = televisao.SintonizadaEm;
+            var canalEsperado = canalAtual + 1;
+
+            televisao.CanalProximo();
+
+            Assert.AreEqual(canalEsperado, televisao.SintonizadaEm);
+        }
+
+        [TestMethod]
+        public void ShouldChangeToPreviousChannel()
+        {
+            Televisao televisao = PegarTelevisao();
+            var canalAtual = televisao.SintonizadaEm;
+            var canalEsperado = canalAtual - 1;
+
+            televisao.CanalAnterior();
+
+            Assert.AreEqual(canalEsperado, televisao.SintonizadaEm);
+        }
+
+        private Televisao PegarTelevisao()
+        {
+            var canalInicial = 0;
+            return new Televisao(canalInicial);
         }
     }
 }
--- Utilizando POO/exercicio04/SalaDeEstar/Televisao.cs
+++ Utilizando POO/exercicio04/SalaDeEstar/Televisao.cs
@@ -2,6 +2,19 @@ namespace SalaDeEstar
 {
     public class Televisao
     {
+        public int SintonizadaEm { get; private set; }
+        public Televisao(int canalInicial)
+        {
+            SintonizadaEm = canalInicial;
+        }
+        public void CanalProximo()
+        {
+            SintonizadaEm++;
+        }
 
+        public void CanalAnterior()
+        {
+            SintonizadaEm--;
+        }
     }
 }
